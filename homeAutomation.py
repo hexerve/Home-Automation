60e8a4b2a86289c41f3e45ce3b192c0900ce737b
JDchauhan <JDchauhan>
2018-11-28 14:11:28 +0530

    complete

--- homeAutomation.py
+++ homeAutomation.py
@@ -1,4 +1,5 @@
 import machine
+import json
 
 pins = [
     machine.Pin(14, machine.Pin.OUT), machine.Pin(12, machine.Pin.OUT), machine.Pin(
@@ -8,10 +9,34 @@ pins = [
 for pin in pins:
     pin.low()
 
+with open("automation.json", "r") as jsonFile:
+    data = json.load(jsonFile)
+    for i in range (0,5):
+        if (data[str(i)] == 0):
+            pins[i].low()
+        else:
+            pins[i].high()
+        
+
 def operate(num, msg):
+    with open("automation.json", "r") as jsonFile:
+        data = json.load(jsonFile)
+
     if(msg == 'ON'):
+        data[str(num)] = 1
         pins[num].high()
+
     if(msg == 'OFF'):
         pins[num].low()
+        data[str(num)] = 0
+
+    data = json.dumps(data)
+
+    print(data)
+    f = open('automation.json', "w")
+    f.write(data)
+    f.close()
 
     print(num, msg)
+
+
